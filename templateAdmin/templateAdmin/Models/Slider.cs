c51bfeaea50a48602c7c89ccf83d8139211fd2b1
Allahverdi1 <99805939+Allahverdi1@users.noreply.github.com>
2022-05-27 10:10:00 +0300

    templateAdmin

---
+++ templateAdmin/templateAdmin/Models/Slider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace templateAdmin.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public int DiscountPercent { get; set; }
+        [Required(ErrorMessage="Bura bosh ola bilmez"), MinLength(10)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [NotMapped, Required]
+        public IFormFile Photo { get; set; }
+    }
+}
